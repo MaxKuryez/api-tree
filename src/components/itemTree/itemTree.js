5c8d874dcb3fbfc5021e08f503adb892742d4d80
Maksim Kuryez <maxkuryez@gmail.com>
2023-02-26 12:08:29 -0800

    change info

--- src/components/itemTree/itemTree.js
+++ src/components/itemTree/itemTree.js
@@ -22,6 +22,8 @@ const ItemTree = () => {
       <br/>
       Note, that the root of the tree cannot be deleted, or edited, only the children can be added there.
       <br />
+      Also, note that when updating the tree I use local functions to edit the tree locally, so that I do not need to send request every time.
+      <br />
       The errors are also handled, so if you change some request to 'delete', or block the request url, you will get an error on page.
       <br />
       You can see what is a child of what by the shift from the left in the tree represenation.
